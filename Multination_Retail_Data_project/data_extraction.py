8da1d768c7fb105b26fbecba1aebdb36f507cc79
Alamdave Gill <alamdave@gmail.com>
2023-12-18 09:58:43 +0000

    completes the listing data tables and reading them in a panda df

--- Multination_Retail_Data_project/data_extraction.py
+++ Multination_Retail_Data_project/data_extraction.py
@@ -10,8 +10,11 @@ class DataExtractor :
             return table_names
     
     def read_rds_table(self, database_con, table_name ):
+        engine = database_con.init_db_engine()
 
-        return True
+        with engine.connect() as conn:
+            df = pd.read_sql_table(table_name, conn)
+            return df
 
 
 #instantiate a data_extractor class
@@ -19,5 +22,13 @@ data_extractor = DataExtractor()
 #instantiate a databaseconnector class from database_utils
 dbc = du.DatabaseConnector()
 
-print(data_extractor.list_db_tables(dbc))
+user_table = data_extractor.list_db_tables(dbc)[1]
+
+print(user_table)
+
+user_data_panda = data_extractor.read_rds_table(dbc, user_table)
+print(user_data_panda)
+# Read the table into a pandas DataFrame
+#user_data_df = data_extractor.read_rds_table(dbc, user_data_table)
+
 
